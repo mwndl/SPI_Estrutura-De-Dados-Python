1ff3175f91835599f9de1ba7f8f87305c16b6659
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2023-10-09 21:34:07 -0300

    Add files via upload

---
+++ Lista_2/Exer (7).py
@@ -0,0 +1,10 @@
+# Questão 7
+# Crie uma classe chamada “Carro” com atributos “marca”, “modelo” e “ano”. Implemente um método chamado “detalhes” que retorna uma string com as informações do carro.
+class Carro:
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+
+    def detalhes(self):
+        return f"Marca: {self.marca}, Modelo: {self.modelo}, Ano: {self.ano}"
